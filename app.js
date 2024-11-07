fff361885b843351a4a3d05015a24448e349a25f
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-07 12:26:59 +0100

    upgrade commit

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const app = express()
 const gattiRoutes = require('./routes/gatti.js')
+const notFoundMiddleware = require('./middlewares/not found.js')
+const loggerMiddleware = require('./middlewares/loggerMiddleware.js')
 app.use(express.json())
 
+const HOST = process.env.HOST
+const PORT = process.env.PORT
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+    console.log(`Server is runnig at ${HOST}:${PORT}`);
 })
+
+
+app.use((err, req, res, next) => {
+    console.log('Error', err.message);
+    console.error(err.stack);
+    res.status(500).send({
+        message: 'Something went wrong',
+        error: err.message
+    })
+    
+})
+
+
+
+app.use('/gatti', loggerMiddleware)
 
 app.use('/gatti', gattiRoutes)
 
+app.use(notFoundMiddleware)
+
+app.use('/gatti', (req, res, next) => {
+    throw new Error('Tou broken everything dude!')
+})
---
+++ middlewares/not found.js
@@ -0,0 +1,6 @@
+const notFoundMiddleware = (req, res, next) => {
+    res.status(404).send("Sorry can't find that!");
+}
+
+
+module.exports = notFoundMiddleware;
